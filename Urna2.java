b8b547ede85915c6359d15d467b21e3217335ef7
Kaina <kainarodrigues684@gmail.com>
2025-05-22 20:34:28 -0300

    Add files via upload

---
+++ Urna2.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.urna2;
+
+/**
+ *
+ * @author 2Ds_tec
+ */
+public class Urna2 {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
